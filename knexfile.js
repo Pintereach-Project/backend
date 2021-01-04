5bfac8179d6c6d2cf0fa8575f1731df3272056f5
Gerardo Rivera <gerardo.rivera3901@gmail.com>
2021-01-04 12:21:33 -0800

    Created production env

--- knexfile.js
+++ knexfile.js
@@ -16,5 +16,24 @@ module.exports = {
         conn.run('PRAGMA foreign_keys = ON', done);
       }
     }
+  },
+
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/pintereach_prod.db3'
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    }
   }
 };
