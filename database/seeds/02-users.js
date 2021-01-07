0b95e86c999c4566d397308c2cc88c1db600b208
Gerardo Rivera <gerardo.rivera3901@gmail.com>
2021-01-06 23:36:43 -0800

    Fixed broken login seeds

--- database/seeds/02-users.js
+++ database/seeds/02-users.js
@@ -1,8 +1,8 @@
 
 exports.seed = function(knex) {
   return knex('users').insert([
-    {email: 'email1', username: 'username1', password: 'password1'},
-    {email: 'email2', username: 'username2', password: 'password2'},
-    {email: 'email3', username: 'username3', password: 'password3'}
+    {email: 'email1', username: 'username1', password: '$2a$08$PE3EheRMl7.uzucQWnh2S.nl86u3bFuAR02JYecJVZL8xjAZbWteC'},
+    {email: 'email2', username: 'username2', password: '$2a$08$pVHytHghU0Pyg0sEHYbNHe18VRPnKneVZ4Q0DLf0s4EAisga2XYZi'},
+    {email: 'email3', username: 'username3', password: '$2a$08$31hoXtrNgS1Ykj8zxB.zPOnDYkNhZ.m3twyp3GWNxh3FIRIYgSLbe'}
   ]);
 };
