4f4bf5db413782f10a813de221cb48edfbfa6dac
Gerardo Rivera <gerardo.rivera3901@gmail.com>
2021-01-07 16:58:05 -0800

    Removed token restriction for testing

--- api/server.js
+++ api/server.js
@@ -7,15 +7,15 @@ const authRouter = require('./auth/auth-router');
 const articleRouter = require('./articles/article-router');
 const savedArticleRouter = require('./savedArticles/savedArticle-router');
 
-const restricted = require('./middleware/restricted');
+// const restricted = require('./middleware/restricted');
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
 server.use('/api/auth', authRouter);
-server.use('/api/articles', restricted, articleRouter);
-server.use('/api/saved-articles', restricted, savedArticleRouter);
+server.use('/api/articles', articleRouter);
+server.use('/api/saved-articles', savedArticleRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'up' });
